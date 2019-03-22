8a5c7948f10a1ac233dc4860baf6288c4c08999f
DESKTOP-Q5VKEQJ\amj19 <amanzanoj@cirsa.com>
2019-03-22 10:57:12 +0100

    (Dirty) Restored restApi project

---
+++ Schibsted.Test.BE/Schibsted.Test.BE/Schibsted.Test.BE.API/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Schibsted.Test.BE.Business.Entities.DTO;
+using Schibsted.Test.BE.Business.Entities.POCO;
+using Schibsted.Test.BE.Business.Interface.Services;
+
+namespace Schibsted.Test.BE.API.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService ?? throw new ArgumentNullException($"{nameof(_authService)} is null");
+        }
+
+        [Route("api/[controller]/login")]
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> Post(LoginCredentials credentials)
+        {
+
+            try
+            {
+                var result = await _authService.AuthenticateUserAsync(credentials.Email, credentials.Password);
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+        [Route("api/[controller]/logout/{email}")]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> Post(string email)
+        {
+
+            try
+            {
+                await _authService.Logout(email);
+                return Ok("Successfully logout");
+
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+    }
+}
---
+++ Schibsted.Test.BE/Schibsted.Test.BE/Schibsted.Test.BE.API/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Schibsted.Test.BE.Business.Entities.DTO;
+using Schibsted.Test.BE.Business.Entities.Entities;
+using Schibsted.Test.BE.Business.Interface.Services;
+
+namespace Schibsted.Test.BE.API.Controllers
+{
+
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService ?? throw new ArgumentNullException($"{nameof(_userService)} is null");
+        }
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> GetAll()
+        {
+
+            try
+            {
+                var result = await _userService.GetAllUsersAsync();
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+        [Route("api/[controller]/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> Get(string id)
+        {
+
+            try
+            {
+                var result = await _userService.GetUserAsync(id);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound($"user with id : {id} was not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+        [Route("api/[controller]")]
+        [HttpPost]
+        public async Task<ActionResult> Post(User user)
+        {
+            try
+            {
+                await _userService.AddUserAsync(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+        [Route("api/[controller]")]
+        [HttpPut]
+        public async Task<ActionResult> Put(User user)
+        {
+            try
+            {
+                await _userService.UpdateUserAsync(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+        [Route("api/[controller]")]
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                await _userService.RemoveUserAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // TODO logError;
+                return Forbid();
+            }
+        }
+    }
+
+}
